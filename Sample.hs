bfbfd928f0ed98cc31e982914f400c43211bf0f1
daiki <d-higa@jaist.ac.jp>
2018-01-19 22:21:45 +0900

    get next and get tail

--- Sample.hs
+++ Sample.hs
@@ -1,12 +1,19 @@
-
 -- IntLinkeListEle supports only interger type of data.
 data IntLinkeListEle = IntLinkeListEle{
     num :: Int,
     next :: Maybe IntLinkeListEle
 } deriving Show
 
--- addEle :: IntLinkeListEle -> Int -> IntLinkeListEle
-addEle list num =  IntLinkeListEle num list :: IntLinkeListEle
+addEle list num index findIndex =  IntLinkeListEle num list :: IntLinkeListEle
+
+getNext :: IntLinkeListEle -> Maybe IntLinkeListEle
+getNext list = next list :: Maybe IntLinkeListEle
+
+getTail :: IntLinkeListEle  -> IntLinkeListEle 
+getTail list = 
+    case getNext list of
+        Nothing -> list
+        Just x -> getTail x
 
 main :: IO ()
 main = do
